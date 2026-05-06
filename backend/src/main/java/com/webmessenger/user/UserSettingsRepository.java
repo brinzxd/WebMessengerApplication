5236c11f18ee98a3b53caa1779d48dc8c37a1d1a
brinzxd <zdanov1chpl@mail.ru>
2026-05-07 00:38:45 +0300

    BE: Fix UserSettingsRepository - add @Query for findByUserId with user.id

--- backend/src/main/java/com/webmessenger/user/UserSettingsRepository.java
+++ backend/src/main/java/com/webmessenger/user/UserSettingsRepository.java
@@ -1,8 +1,10 @@
 package com.webmessenger.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserSettingsRepository extends JpaRepository<UserSettings, Long> {
+    @Query("SELECT s FROM UserSettings s WHERE s.user.id = :userId")
     Optional<UserSettings> findByUserId(Long userId);
 }
