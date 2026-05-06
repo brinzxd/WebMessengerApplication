dc14a1b141e4fd8b018550fc65587c069855463b
brinzxd <zdanov1chpl@mail.ru>
2026-05-06 19:33:20 +0300

    M1: Add Spring Boot entry point

---
+++ backend/src/main/java/com/webmessenger/WebMessengerApplication.java
@@ -0,0 +1,11 @@
+package com.webmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMessengerApplication.class, args);
+    }
+}
