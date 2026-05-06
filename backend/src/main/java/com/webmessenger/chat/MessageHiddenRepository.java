226eb8d598c68eeb8afc4dd592e4bd7f990f3aff
brinzxd <zdanov1chpl@mail.ru>
2026-05-07 00:03:14 +0300

    Fix 3a: Add MessageHiddenRepository.java

---
+++ backend/src/main/java/com/webmessenger/chat/MessageHiddenRepository.java
@@ -0,0 +1,6 @@
+package com.webmessenger.chat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageHiddenRepository extends JpaRepository<MessageHidden, Long> {
+}
